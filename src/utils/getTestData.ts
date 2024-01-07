1853f8f605b7a7c9dfd9876aecf927eb6fee4c10
Amir Beshkar <amir.beshkar@gmail.com>
2024-01-07 15:10:07 +0100

    add a util to provide data for unit testing

---
+++ src/utils/getTestData.ts
@@ -0,0 +1,78 @@
+import { z } from 'zod';
+import { Options } from '../types/Options';
+import { FailurePolicy } from '../types/FailurePolicy';
+import { getDefaultValue } from './getDefaultValue';
+
+export const SAMPLE_KEY = 'sample-key';
+
+export function getTestData({
+  withDefaultValue = false,
+  failurePolicy = 'exception',
+}: {
+  withDefaultValue?: boolean;
+  failurePolicy?: FailurePolicy;
+} = {}) {
+  const schema = z.object({ id: z.number(), label: z.string() });
+  const schemaWithDefault = schema.default({ id: 1, label: 'default value' });
+
+  return createTestData({
+    schema: withDefaultValue ? schemaWithDefault : schema,
+    value: { id: 99, label: 'data in storage' },
+    failurePolicy,
+  });
+}
+
+interface TestDataOptions<Schema extends z.ZodTypeAny> {
+  schema: Schema;
+  value: z.infer<Schema>;
+  failurePolicy: FailurePolicy;
+}
+
+interface TestData<Schema extends z.ZodTypeAny> {
+  defaultValue: z.infer<Schema> | undefined;
+  value: z.infer<Schema>;
+  storedValue: {
+    ok: string;
+    decoderIncompatible: string;
+    schemaIncompatible: string;
+  };
+  options: Options<Schema>;
+}
+
+const encoder = JSON.stringify;
+const decoder = JSON.parse;
+
+function createTestData<Schema extends z.ZodTypeAny>({
+  schema,
+  value,
+  failurePolicy,
+}: TestDataOptions<Schema>): TestData<Schema> {
+  return {
+    defaultValue: getDefaultValue(schema),
+    value,
+    storedValue: {
+      ok: encoder(value),
+      decoderIncompatible: 'something incompatible with what decoder expects',
+      schemaIncompatible: encoder(true),
+    },
+    options: {
+      decoder,
+      encoder,
+      schema,
+      transformDecodedValue: value => value,
+      storage: {
+        getItem: jest.fn(),
+        setItem: jest.fn(),
+        removeItem: jest.fn(),
+      },
+      logger: {
+        error: jest.fn(),
+      },
+      failurePolicy: {
+        decodeError: failurePolicy,
+        encodeError: failurePolicy,
+        schemaCheck: failurePolicy,
+      },
+    },
+  };
+}
