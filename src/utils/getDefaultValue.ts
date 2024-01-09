387939184c8372dd5808e1a3f3cf731df3406950
Amir Beshkar <amir.beshkar@gmail.com>
2024-01-09 22:43:34 +0100

    improve getDefaultValue to detect default value without zod classes

--- src/utils/getDefaultValue.ts
+++ src/utils/getDefaultValue.ts
@@ -3,9 +3,15 @@ import { z } from 'zod';
 export function getDefaultValue<Schema extends z.ZodTypeAny>(
   schema: Schema
 ): z.infer<Schema> | undefined {
-  if (schema instanceof z.ZodDefault) {
+  if (isInstanceOfZodDefault(schema)) {
     return schema._def.defaultValue();
   }
 
   return undefined;
+}
+
+function isInstanceOfZodDefault(
+  schema: unknown
+): schema is z.ZodDefault<z.ZodTypeAny> {
+  return typeof (schema as z.ZodTypeAny)._def.defaultValue === 'function';
 }
