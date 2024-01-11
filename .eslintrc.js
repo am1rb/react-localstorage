bfaf911c7d5d3f5335cdd47e5a513ad8ecd57e59
Amir Beshkar <amir.beshkar@gmail.com>
2024-01-11 23:03:51 +0100

    install more linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,8 @@ const options = {
     'plugin:prettier/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
   ],
 };
 
